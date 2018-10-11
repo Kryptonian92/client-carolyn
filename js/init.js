9dd17aedd4ba0b2f063fc31684f310dc61b409c3
Ausar McGruder <ausar_mcgruder@yahoo.com>
2018-10-10 22:48:06 -0400

    Updated clients section

--- js/init.js
+++ js/init.js
@@ -4,6 +4,11 @@
     $('.sidenav').sidenav();
     $('.parallax').parallax();
 
+// Slider text
+$(document).ready(function(){
+  $('.slider').slider();
+});
+
   }); // end of document ready
 })(jQuery); // end of jQuery name space
 
